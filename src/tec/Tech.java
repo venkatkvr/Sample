db33ab20afdb7b2e5499613c049c7bb8097362ce
venkat <kvratg1006@gmail.com>
2015-05-17 17:31:42 +0530

    My first commit

---
+++ src/tec/Tech.java
@@ -0,0 +1,14 @@
+package tec;
+
+public class Tech {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi,");
+
+	}
+
+}
