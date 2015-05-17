3cb6629b74198ae87abaac17ae78ef5f5f6a79fd
venkat <kvratg1006@gmail.com>
2015-05-17 17:59:38 +0530

    my 3rd commit

--- src/tec/Tech.java
+++ src/tec/Tech.java
@@ -7,7 +7,7 @@ public class Tech {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi, Welcome to the new programing world.");
+		System.out.println("Hi, Welcome to the new programing world. & Have a fun.");
 
 	}
 
